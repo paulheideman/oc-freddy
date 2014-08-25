658e5a4470757fc1fa0012736f9b3827ba14c203
Paul Heideman <paul.heideman@ocado.com>
2014-08-25 10:54:02 +0100

    Some helper functions

--- src/oc_freddy/bot.clj
+++ src/oc_freddy/bot.clj
@@ -1,28 +1,29 @@
 (ns oc-freddy.bot
   (:use [oc-freddy.core]))
 
-(defn hero-health [input]
-  (:life (:hero input)))
+(defn hero-health [input] (:life (:hero input)))
+(defn board [input] (:board (:game input)))
+(defn hero-pos [input] (:pos (:hero input)))
 
 (defn switch-to-acquire-mine [input]
-  (let [mine (closest-capturable-mine (:board (:game input)) (:pos (:hero input)) (:id (:hero input)))]
+  (let [mine (closest-capturable-mine (board input) (hero-pos input) (:id (:hero input)))]
     (prn "mine" mine)
     (if (empty? mine) {:state :random}
       {:state :acquire-mine :pos mine})))
 
 (defn switch-to-get-health [input]
-  (let [beer (closest-beer (:board (:game input)) (:pos (:hero input)))]
+  (let [beer (closest-beer (board input) (hero-pos input))]
     (if (nil? beer) {:state :random}
       {:state :full-health :pos beer})))
 
 (defn go-to-mine [input state]
-  (let [path      (simple-path (:board (:game input)) (:pos (:hero input)) (:pos state))
+  (let [path      (simple-path (board input) (hero-pos input) (:pos state))
         direction (second path)
         distance  (first path)]
     [direction (if (> distance 1) state (switch-to-get-health input))]))
 
 (defn go-to-health [input state]
-  (let [path      (simple-path (:board (:game input)) (:pos (:hero input)) (:pos state))
+  (let [path      (simple-path (board input) (hero-pos input) (:pos state))
         direction (second path)
         distance  (first path)]
     [direction (if (or (> distance 1) (< (hero-health input) 96))
