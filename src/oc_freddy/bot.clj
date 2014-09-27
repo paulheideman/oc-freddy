cbea011bf93d8051d7687c426bcd6337e7ea52bb
Paul Heideman <paul.heideman@ocado.com>
2014-09-27 16:52:44 +0100

    Broken commit

--- src/oc_freddy/bot.clj
+++ src/oc_freddy/bot.clj
@@ -17,81 +17,82 @@
 
 (defn money? [input] (> (hero-gold input) 0))
 
-(defn make-return [direction action & ps]
+(defn make-return [state direction action & ps]
   (prn "make-return" direction action)
   [direction (into {:action action} (map (partial apply vector) (partition 2 ps)))])
 
-(defn not-closer-to-beer [input h]
-  (> (or (:distance (closest-beer (board input) (:pos h))) Integer/MAX_VALUE)
-     (or (:distance (closest-beer (board input) (hero-pos input))) Integer/MAX_VALUE)))
+(defn not-closer-to-beer [input paths h]
+  (> (or (:distance (closest-beer (board input) paths (:pos h))) Integer/MAX_VALUE)
+     (or (:distance (closest-beer (board input) paths (hero-pos input))) Integer/MAX_VALUE)))
 
-(defn within-one? [board pos h]
-  (< (simple-path-distance board pos (:pos h)) 3))
+(defn within-one? [board paths pos h]
+  (< (or (:distance (simple-path paths pos (:pos h))) Integer/MAX_VALUE) 3))
 
-(defn not-close-enough-to-beer [input h]
-  (< (or (:distance (closest-beer (board input) (:pos h))) Integer/MAX_VALUE) (/ (:life h) 20)))
+(defn not-close-enough-to-beer [input paths h]
+  (< (or (:distance (closest-beer (board input) paths (:pos h))) Integer/MAX_VALUE) (/ (:life h) 20)))
 
-(defn vulnerable-enemy [input h]
+(defn vulnerable-enemy [input paths h]
   (and (< (:life h) (- (hero-life input) 20))
-       (or (and (within-one? (board input) (hero-pos input) h) (not-close-enough-to-beer input h))
-           (not-closer-to-beer input h))))
+       (or (and (within-one? (board input) paths (hero-pos input) h) (not-close-enough-to-beer input paths h))
+           (not-closer-to-beer input paths h))))
 
-(defn vulnerable-enemies [input]
-  (filter (partial vulnerable-enemy input) (vals (heroes input))))
+(defn vulnerable-enemies [input paths]
+  (filter (partial vulnerable-enemy input paths) (vals (heroes input))))
 
-(defn kill-enemy [input]
-  (let [targets    (vulnerable-enemies input)
+(defn kill-enemy [input state]
+  (let [targets    (vulnerable-enemies input (:paths state))
         paths      (map #(safe-path (board input) (hero-pos input) (:pos %) (hero-id input)
                                     (- (hero-life input) 20) (heroes input)) targets)
         path       (first (sort-by :distance (filter (comp not nil?) paths)))]
     (if-not (empty? path)
-      (make-return (:direction path) :kill-enemy :target (:destination path)))))
+      (make-return state (:direction path) :kill-enemy :target (:destination path)))))
 
 (defn tavern-neighbors [input]
   (map :pos (filter #(= (:tile %) :tavern)
                     (map (partial tile-at (board input)) (neighbors-of (board-size input) (hero-pos input))))))
 
-(defn get-full-health [input]
+(defn get-full-health [input state]
   (let [beer (tavern-neighbors input)]
     (if (and (< (hero-life input) 99) (not (empty? beer)))
-      (make-return
+      (make-return state
         (first (map (partial direction-to (hero-pos input))
                     (shuffle beer)))
         :get-full-health))))
 
-(defn spar-enemy [input]
+(defn spar-enemy [input state]
   nil)
 
-(defn go-to-mine [input unsafe-locations]
+(defn go-to-mine [input unsafe-locations state]
   (let [safe-mine (closest-safe-capturable-mine (board input) (hero-pos input) (hero-id input) unsafe-locations)]
     (if (> (- (hero-life input) (or (:distance safe-mine) Integer/MAX_VALUE)) 20)
       (and safe-mine
-        (make-return (:direction safe-mine) :go-to-mine :destination (:destination safe-mine))))))
+        (make-return state (:direction safe-mine) :go-to-mine :destination (:destination safe-mine))))))
 
-(defn go-to-beer [input unsafe-locations]
+(defn go-to-beer [input unsafe-locations state]
   (if (and (money? input) (not (full-health? input)))
     (let [safe-beer (closest-safe-beer (board input) (hero-pos input) unsafe-locations)]
-      (and safe-beer (make-return (:direction safe-beer) :go-to-beer :destination (:destination safe-beer))))))
+      (and safe-beer (make-return state (:direction safe-beer) :go-to-beer :destination (:destination safe-beer))))))
 
-(defn go-to-spawn [input unsafe-locations]
+(defn go-to-spawn [input unsafe-locations state]
   (let [direction (:direction (safe-path (board input) unsafe-locations (hero-pos input) (hero-spawn-pos input)))]
-    (and direction (make-return direction :go-to-spawn))))
+    (and direction (make-return state direction :go-to-spawn))))
 
-(defn suicide [input]
+(defn suicide [input state]
   (if (zero? (hero-mine-count input))
-    (make-return (:direction (closest-enemy-or-mine (board input) (hero-pos input) (hero-id input))) :suicide)))
+    (make-return state (:direction (closest-enemy-or-mine (board input) (hero-pos input) (hero-id input))) :suicide)))
 
-(defn run [input unsafe-locations scary-enemies]
-    (make-return (:direction (run-path (board input) (hero-pos input) unsafe-locations scary-enemies)) :run))
+(defn run [input unsafe-locations scary-enemies state]
+    (make-return state (:direction (run-path (board input) (hero-pos input) unsafe-locations scary-enemies)) :run))
 
-(defn bot [input]
+(defn bot [input state]
   (let [unsafe-locations (unsafe-locations (board input) (hero-id input) (hero-life input) (heroes input))
-        scary-enemies    (scary-enemy-locations (board input) (hero-id input) (hero-life input) (heroes input))]
-    (or (kill-enemy input)
-        (spar-enemy input)
-        (get-full-health input)
-        (go-to-mine input unsafe-locations)
-        (go-to-beer input unsafe-locations)
-        (go-to-spawn input unsafe-locations)
-        (suicide input)
-        (run input unsafe-locations scary-enemies))))
+        scary-enemies    (scary-enemy-locations (board input) (hero-id input) (hero-life input) (heroes input))
+        state            {:paths (get state :paths (all-paths (board input)))}]
+    (or (kill-enemy input state)
+        (spar-enemy input state)
+        (get-full-health input state)
+        (go-to-mine input unsafe-locations state)
+        (go-to-beer input unsafe-locations state)
+        (go-to-spawn input unsafe-locations state)
+        (suicide input state)
+        (run input unsafe-locations scary-enemies state))))
--- src/oc_freddy/client.clj
+++ src/oc_freddy/client.clj
@@ -29,7 +29,9 @@
 
 (defn parse-tiles [tiles] (to-array (with-positions (map parse-tile (partition 2 (seq tiles))))))
 
-(defn parse-input [input] (update-in input [:game :board :tiles] parse-tiles))
+(defn parse-input [input]
+  ;(prn input)
+  (update-in input [:game :board :tiles] parse-tiles))
 
 (defn request [url, params]
   "makes a POST request and returns a parsed input"
@@ -48,10 +50,9 @@
 
 (defn step [from]
   (loop [input from
-         state {:start "start"}]
+         state {}]
     (print "(" (:turn (:game input)) "/" (:maxTurns (:game input)) "-" (:life (:hero input)) ")  - ")
-    (prn state)
-    (let [result    (bot input)
+    (let [result    (bot input state)
           dir       (first result)
           new-state (second result)
           next      (request (:playUrl input) {:dir (get dir-map dir "stay")})]
