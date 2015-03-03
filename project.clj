eedf8f2c9b80f5ecdcb97095379b3a80d68c6ac9
Paul Heideman <paul@paulheideman.com>
2015-03-03 21:29:10 +0000

    New clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [clj-http "0.7.8"]
                  [cheshire "5.3.1"]
                  [slingshot "0.10.3"]
