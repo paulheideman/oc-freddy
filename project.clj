aabf6b2873ce9708305506f14b011797c0885c57
Paul Heideman <paul@paulheideman.com>
2015-03-03 21:40:19 +0000

    Updated deps

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [clj-http "0.7.8"]
-                 [cheshire "5.3.1"]
+                 [clj-http "1.0.1"]
                  [slingshot "0.10.3"]
                  [aysylu/loom "0.5.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
